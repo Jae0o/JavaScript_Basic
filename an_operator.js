4ec8f32a4278db9c016d7810a0d90f58f1da462b
JanAcBuJoc <leey153@naver.com>
2023-03-30 18:16:46 +0900

    study_an operator

---
+++ an_operator.js
@@ -0,0 +1,21 @@
+//  " = " [ 대입연산자 ] 좌의 값을 우에 대입한다 
+let A = "1";
+
+// [ 비교연산자 ]
+// "=="  [ 동등 연산자 ] 좌우를 비교해 같다면 true 아니면 false
+// "===" [ 일치 연산자 ] 좌우를 비교해 데이터 형식까지 같다면 true 아니라면 false
+
+// "!" [ 부정 ]
+// ex) 1! = 2   / true 
+
+// "&&" [ 논리연산자 / and ]
+// ture && true : 실행
+// ture && false : X
+// false && true : X
+// false && false : X
+
+// "||" [ 논리 연산자 / or ] enter key / backspace key 사이의 |
+// ture && true : 실행
+// ture && false : 실행
+// false && true : 실행
+// false && false : 실행
