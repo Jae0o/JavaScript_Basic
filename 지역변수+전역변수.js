e806657773881c5857bf48919a179d00db2d8867
JanAcBuJoc <leey153@naver.com>
2023-04-06 23:24:51 +0900

    수정

--- 지역변수+전역변수.js
+++ 지역변수+전역변수.js
@@ -10,10 +10,10 @@
 /* 예제 1 */
 
 function test() {
-    let veriable = "local";
-    console.log(veriable);
+    let variable = "local";
+    console.log(variable);
 };
-let veriable = "global";
+let variable = "global";
 test();
 // 결과는 local 함수의 안에 있는 지역변수를 우선시한다.
 // 전역변수가 지정 되어있다 하더라도 함수안의 지역변수와 같은 변수가 있다면 지역변수가 적용되어진다.
@@ -23,9 +23,9 @@ test();
 /* 예제 2 */
 
 function test2() {
-    let veriable2 = "local";
-    let Lv = "local veriable";
-    console.log(veriable2);
+    let variable2 = "local";
+    let Lv = "local variable";
+    console.log(variable2);
 };
 test2();
 // /* console.log(Lv); */ = 결과는 undefined
@@ -35,12 +35,12 @@ test2();
 
 /* 예제 3 */
 
-let veriable3 = "global";
+let variable3 = "global";
 function test3() {
-    veriable3 = 'local';
+    variable3 = 'local';
 };
 test3();
-console.log(veriable3);
+console.log(variable3);
 /* var let const 같은 선언자가 붙어 있지 않다면 지역변수로 작용하는것이 아니라
 이전에 지정한 전역 변수를 대상으로 변경하는것과 같기 때문에 전역변수가 변하게 된다. */
 
