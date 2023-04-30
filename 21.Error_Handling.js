8316dc11c466f1b532f74537a218140b638fb824
Lee Jae Yeong <127748428+JanAcBuJoc@users.noreply.github.com>
2023-04-30 16:48:00 +0900

    Study and Practice
    Bubbling

--- 21.Error_Handling.js
+++ 21.Error_Handling.js
@@ -36,3 +36,45 @@ TryCatchFinally(Error());
 // 에러가 발생할것 같은 function,code 라면 "try" 로 시도 하고
 // 에러 발생시 "catch(error)"안의 내용이 실행이 된다.
 // 이후 "finally"에서 이전의 값이 성공, 실패의 유무와는 무관하게 리소스 정리등이 가능하다.
+
+
+/* ------------------------------------------------------------------------ */
+
+/* Error Bubbling */
+// 에러는 전파되어 질 수 있음
+// 또 에러가 발생한 함수외의 다른함수에서 중간에 에러를 잡아줄 수 있음.
+
+function A() {
+    throw new Error("error");
+};
+// A에서 에러가 발생
+function B() {
+    try {
+        A();
+        // B에서 A를 호출하면 Error가 전파되어지고 이를 중간에 잡아 줄수 있다.
+    } catch (error) {
+        console.log("B Error Bubbling!");
+        console.log("B에서 try를 통해 미리 에러를 검출");
+    } finally {
+        console.log("B에서의 try catch Finally");
+    };
+};
+
+function C() {
+    try {
+        B();
+    } catch (error) {
+        console.log('C Error Bubbling!');
+        // 앞의 B의 try catch finally를 주석처리하면 C에서 Error를 캐치한다.
+        // 즉 원하는 위치에서 에러를 캐치해 잡아낼 수 있음.
+    } finally {
+        console.log('C에서의 try catch Finally');
+        // B에서 Error를 잡아 냈더라도 C의 finally는 잘 작동한다.
+    }
+};
+C();
+/* 결과
+B Error Bubbling!
+B에서 try를 통해 미리 에러를 검출
+B에서의 try catch Finally
+C에서의 try catch Finally */
